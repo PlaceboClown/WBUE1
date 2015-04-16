a9fd164b48829542b45b912f1c72f2526d3942a7
lukas <e1325082@student.tuwien.ac.at>
2015-04-16 12:34:46 +0200

    jsp files

---
+++ ue2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/BigJeopardyservlet.java
@@ -0,0 +1,39 @@
+package at.ac.tuwien.big.we15.lab2.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BigJeopardyservlet
+ */
+@WebServlet("/BigJeopardyservlet")
+public class BigJeopardyservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BigJeopardyservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
